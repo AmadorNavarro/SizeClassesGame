ef8e86b00f032431f9f64372a41c26e46e91ba2c
Amador Navarro Lucas <macmaker@me.com>
2016-02-15 22:54:36 +0100

    initCommit

---
+++ SizeClassesGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SizeClassesGame
+//
+//  Created by Amador Navarro Lucas on 15/02/16.
+//  Copyright © 2016 Amador Navarro Lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SizeClassesGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SizeClassesGame
+//
+//  Created by Amador Navarro Lucas on 15/02/16.
+//  Copyright © 2016 Amador Navarro Lucas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
